3b187c78c3e7cb4aadff4658edd84cb7a77e84d1
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2020-10-14 17:39:17 +0100

    Ajout de de l'abonnement dans les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Enterprise;
 use Faker;
 use DateTime;
 use App\Entity\Role;
+use App\Entity\Subscription;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -35,6 +36,26 @@ class AppFixtures extends Fixture
         $superAdminRole->setTitle('ROLE_SUPER_ADMIN');
         $manager->persist($superAdminRole);
 
+        $abonnementName = ['KnD Bill Basic', 'KnD Bill Premium', 'KnD Bill & Stock Basic', 'KnD Bill & Stock Premium One'];
+        $sheetNb = [200, 19022018, 200, 19022018];
+        $refNb  = [0, 0, 50, 50];
+        $tarifs = [
+            '{"1": 4000,"6": 24000,"12": 45000}',
+            '{"1": 99000,"6": 59400,"12": 110000}', '{"1": 12400,"6": 74400,"12": 140000}',
+            '{"1": 16000,"6": 96000,"12": 190000}',
+        ];
+        $subs = [];
+        foreach ($tarifs as $key => $value) {
+            $sub = new Subscription();
+            $sub->setName($abonnementName[$key])
+                ->setTarifs(json_decode($value))
+                ->setSheetNumber($sheetNb[$key])
+                ->setProductRefNumber($refNb[$key]);
+
+            $subs[] = $sub;
+            $manager->persist($sub);
+        }
+
         $enterprise = new Enterprise();
         $enterprise->setSocialReason('LEELOU BABY FOOD SAS')
             ->setNiu('M 042014440616 M')
@@ -42,7 +63,9 @@ class AppFixtures extends Fixture
             ->setAddress('Bépanda Camtel, BP : 2702 Douala')
             ->setPhoneNumber('+237 694342007')
             ->setTva(19.25)
-            ->setEmail('contact@leeloubabyfood.com');
+            ->setEmail('contact@leeloubabyfood.com')
+            ->setSubscription($subs[2])
+            ->setSubscriptionDuration(1);
         $manager->persist($enterprise);
 
         //$date = new DateTime(date('Y-m-d H:i:s'));
