df39f13e3cadafc1bc154b51528c4430c53ca444
PASCAL ALHADOUM <alhadoumpascal@gmail.com>
2020-10-16 18:21:28 +0100

    Correction dans la suppression d'un inventaire

--- src/Migrations/Version20201016171808.php
+++ src/Migrations/Version20201016171808.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201003074012 extends AbstractMigration
+final class Version20201016171808 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,16 +20,12 @@ final class Version20201003074012 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE commercial_sheet ADD duration INT DEFAULT NULL, DROP periodofvalidity');
+        $this->addSql('DROP TABLE migration_versions');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-
-        $this->addSql('ALTER TABLE commercial_sheet ADD periodofvalidity DATETIME DEFAULT NULL, DROP duration');
+        $this->addSql('CREATE TABLE migration_versions (version VARCHAR(14) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`, executed_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(version)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
     }
 }
